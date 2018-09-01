a4b39581be5e24ad48373c2c1d3ee2a197da74b8
James Craig <jmscraig@users.noreply.github.com>
2018-09-01 10:50:09 -0500

    Normalize all the line endings
    CRLF corrections

---
+++ packages/rocketchat-ui/client/lib/customEventPolyfill.js
@@ -0,0 +1,12 @@
+(function() {
+	function CustomEvent(event, params) {
+		params = params || { bubbles: false, cancelable: false, detail: undefined };
+		const evt = document.createEvent('CustomEvent');
+		evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+		return evt;
+	}
+
+	CustomEvent.prototype = window.Event.prototype;
+
+	window.CustomEvent = CustomEvent;
+}());
